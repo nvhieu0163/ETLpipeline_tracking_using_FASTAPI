7f3a6e10c947a009dcd9c4de14e8eda913dacf94
Airflow user <nguyenvuhieu2015@gmail.com>
2023-11-13 11:38:30 +0700

    update test_api.py for testing Flask API

--- test_api.py
+++ test_api.py
@@ -4,14 +4,17 @@ import requests
 
 # curl -d '{"title":"my title", "message":"my message", "uidUsername":"my name", "guidGroupNameAlias":"DWH_JOB_NOTI"}' -X POST -H 'token: 0735fd9182d039594c4fa435505986db7d15BHea' -H 'Content-Type: application/json' --insecure http://10.210.12.92:8688/dataApi/dLinkNoti/group 
 
-url = 'http://127.0.0.1:8000/job/import?job_name=Northwind_orders_SyncJob'
+##########################################################################################
+# TEST using FastAPI
+##########################################################################################
 
-response = requests.get(url=url)
-print(response.text)
+# url = 'http://127.0.0.1:8688/job/import?job_name=Northwind_orders_SyncJob'
+# response = requests.get(url=url)
+# print(response.text)
+# print(response.status_code)
 
 
-
-#############################################
+#############
 # params = {
 #   "title": "Đây là title",
 #   "message": "Đây là message",
@@ -24,8 +27,8 @@ print(response.text)
 #     'Content-Type' : 'application/json'
 # }
 
-# CHANNEL_URL = 'http://127.0.0.1:8000/noti/dlink'
-
+# CHANNEL_URL = 'http://127.0.0.1:8689/noti/dlink'
+# res = requests.get("http://127.0.0.1:8689/get-token")
 # res = requests.post(CHANNEL_URL, headers=headers, json=params)
 
 # if res.status_code != 200:
@@ -33,3 +36,39 @@ print(response.text)
 #     exit(1)
 
 # print(res.text)
+
+
+##########################################################################################
+# TEST using Flask
+##########################################################################################
+
+# url = 'http://127.0.0.1:8688/job/import?job_name=Northwind_orders_SyncJob'
+# url = 'http://127.0.0.1:8688/get-token'
+# response = requests.get(url=url)
+# print(response.text)
+# print(response.status_code)
+
+
+#############
+params = {
+  "title": "Đây là title",
+  "message": "Đây là message",
+  "uidUsername": "hieu.nguyenvu",
+  "guidGroupNameAlias": "group_name"
+}
+
+headers = {
+    'token' : 'ZKRrWBKgRrOJ5hNF7ti1t4tqGfipSuhXbA6VGtlH',
+    'Content-Type' : 'application/json'
+}
+
+CHANNEL_URL = 'http://127.0.0.1:8689/noti/telegram'
+
+res = requests.get("http://127.0.0.1:8689/get-token")
+print(res.text)
+res = requests.post(CHANNEL_URL, headers=headers, json=params)
+print(res.text)
+
+if res.status_code != 200:
+    print('Noti to REPORT_API failed. Will try again')
+    exit(1)
